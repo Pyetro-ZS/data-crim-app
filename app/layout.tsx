9a179919129ee5b452dd34363db2802c5c7a2c4b
Pyetro Stefan <pyetromesquita15@gmail.com>
2025-11-01 13:06:30 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <link rel="icon" href="/icon-logo.png" style="width:500px;height:500px;border-radius:25%;/>
+      <div style="width:500px;height:500px;border-radius:25%;>
+        <link rel="icon" href="/icon-logo.png"/>
+      </div>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased`}>
         <ErrorBoundary>
           <ThemeProvider>
